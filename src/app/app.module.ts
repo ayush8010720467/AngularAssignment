7639882371e5c7451cfb6b585ca6152226f9e199
ayush8010720467 <ayushkatiyar096@gmail.com>
2019-03-26 00:44:49 +0530

    The date picker component and the checkbox component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatePickerComponent } from './date-picker/date-picker.component';
+import { CheckBoxComponent } from './check-box/check-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatePickerComponent,
+    CheckBoxComponent
   ],
   imports: [
     BrowserModule,
