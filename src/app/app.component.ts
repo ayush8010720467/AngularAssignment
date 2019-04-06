3682a8875cd8f56121ea5238255e3a573dc8f6c2
ayush8010720467 <ayushkatiyar096@gmail.com>
2019-04-07 01:05:10 +0530

    the moddel object for the response fo the API made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,45 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private QuestionsDIFJ: Data[];
   private apiUrl = 'https://ca.platform.simplifii.xyz/api/v1/static/assignment5';
   constructor(private route: Router, private http: HttpClient) {
     this.hitApiForQuestionsForDIFJ();
   }
   hitApiForQuestionsForDIFJ() {
     this.http.get(this.apiUrl).subscribe((response) => {
-      console.log(response);
-      console.log('got the response');
+      // console.log(response['response']['data'][0]);
+      this.QuestionsDIFJ = response['response']['data'];
+      console.log(this.QuestionsDIFJ);
+      this.submit();
     });
   }
+  submit() {
+    console.log('the function is called');
+    console.log(this.QuestionsDIFJ);
+  }
+}
+interface Validation {
+  name: string;
+  message: string;
+  value: string;
+}
+interface ShowIf {
+  field: string;
+  value: string;
+}
+interface Api {
+  uri: string;
+  method: string;
+  authEnabled: boolean;
+}
+interface Data {
+  type: string;
+  lable: string;
+  inputType: string;
+  name: string;
+  validations: Validation[];
+  showIf: ShowIf;
+  action: string;
+  api: Api;
 }
