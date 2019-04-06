8f11e3f6924e2e7b88700e67e7bbabc2e9ad0d2a
ayush8010720467 <ayushkatiyar096@gmail.com>
2019-04-06 17:57:25 +0530

    Removed the datepicker component and the checkbox component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DatePickerComponent } from './date-picker/date-picker.component';
-import { CheckBoxComponent } from './check-box/check-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DatePickerComponent,
-    CheckBoxComponent
   ],
   imports: [
     BrowserModule,
